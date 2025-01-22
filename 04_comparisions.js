1f83cd4746ebc16c58deadcb33bb72bfea0e2f76
Devang Pareek <devangpareek29@gmail.com>
2025-01-22 19:59:15 +0530

    data type conversion

---
+++ 04_comparisions.js
@@ -0,0 +1,40 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+/*Note 
+
+=> The reason is that an equality check == and comparions ><>=<= work differently comparisons convert null to a number , treating it as 0.
+That why (3) null >=0 is true ans (1) null > 0 is false.
+
+*/
+
+
+
+
+
+
+
+
+/*The reason is that an equality cheak == and comparisons > <> =
+<= work differently .
+comparisons CONVERT null to a number , treating it as 0.
+That's why (3) null >=0 is true and (1) null > 0 is false.*/
